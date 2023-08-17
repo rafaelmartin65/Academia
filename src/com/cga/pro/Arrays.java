7ad71a41826b2b4c5397867d93b14a527bb2aee8
rafaelmartin65 <rmartin65@gmail.com>
2023-08-17 18:43:10 +0100

    update branch

---
+++ src/com/cga/pro/Arrays.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.cga.pro;
+
+/**
+ * @author Alumno
+ *
+ */
+public class Arrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Definir variables
+		int [] numeros;
+		numeros = new int [3];
+		numeros[2] = 20;
+		System.out.println("salir");
+
+	}
+
+}
